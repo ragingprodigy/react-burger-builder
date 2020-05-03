674a91a67a4e079658b942e812848b2bc325c2c9
Oladapo Omonayajo <o.omonayajo@gmail.com>
2020-05-03 23:14:57 +0800

    Added signup

--- src/store/actions/auth.ts
+++ src/store/actions/auth.ts
@@ -1,5 +1,6 @@
 import { AUTH_START, AUTH_SUCCESS, AUTH_FAILED } from './actionTypes';
 import { TAuthAction } from '@burger/interfaces/auth/authAction';
+import Axios from 'axios';
 
 const authStart = (): TAuthAction => ({ type: AUTH_START });
 
@@ -10,5 +11,15 @@ const authFailed = (error: any): TAuthAction => ({ type: AUTH_FAILED, error });
 export const auth = (email: string, password: string) => {
   return (dispatch: any) => {
     dispatch(authStart());
+    const payload = { email, password, returnSecureToken: true };
+    Axios.post(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBR4qbTs4NunvTEj-AQZPei_4vgqVpYW58`, payload
+    ).then(r => {
+      console.log(r);
+      dispatch(authSuccess(r.data));
+    }).catch(e => {
+      console.log(e);
+      dispatch(authFailed(e));
+    });
   }
 };
